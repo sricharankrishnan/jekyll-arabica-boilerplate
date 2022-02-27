0be6c56a36952fa80f4cba70ed3e75d68e9cad29
sricharankrishnan <sricharan.krishnan@gmail.com>
2022-02-27 18:47:49 +0530

    [Build] New Boilerplate
    Jekyll Lava Boiler Plate now renamed as 'Jekyll Arabica Boilerplate'. Cleaned up directories and
    file dependencies. Layout file is much better with include partials inside it. Added common.js and
    common.scss files

---
+++ app-cleanup.sh
@@ -0,0 +1,5 @@
+# remove the origin remote and keep the scaffold ready for use
+git remote remote origin
+rm -v ./README.md
+echo "[Jekyll Arabica Boilerplate]: Ready for Use!"
+rm -v ./app-cleanup.sh
